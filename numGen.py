e3a3a095d70e19aeed957bb96644f2b0092a56c1
Dan <itdudeislit@gmail.com>
2021-11-02 17:01:31 -0500

    GUI

--- numGen.py
+++ numGen.py
@@ -1,9 +1,10 @@
+# A number generator
 import tkinter as tk
 
+#configure the window
 window = tk.Tk()
 window.geometry("600x600")
 window.title("Number Generator")
-
 window.configure(background='yellow')
 
 window.mainloop()
